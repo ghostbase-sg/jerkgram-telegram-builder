3f6ec2a0d09bf56b25c7acf8a94e438573e8e72a
ghostbase-sg <cronusk1809@gmail.com>
2026-09-14 21:08:48 +0300

    M1: add localization interface

---
+++ Jerkgram/Localization/JGStrings.h
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+NS_ASSUME_NONNULL_BEGIN
+FOUNDATION_EXPORT NSString *JGString(NSString *key);
+FOUNDATION_EXPORT NSString *JGLanguageCode(void);
+NS_ASSUME_NONNULL_END
