37851ce041df2f6e419eaaefd95f830022147242
ghostbase-sg <cronusk1809@gmail.com>
2026-09-14 21:08:52 +0300

    M1: add Settings UI interface

---
+++ Jerkgram/UI/JGSettingsViewController.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+@interface JGSettingsViewController : UITableViewController
+- (instancetype)initWithAccountPeerId:(int64_t)peerId;
+@end
+NS_ASSUME_NONNULL_END
